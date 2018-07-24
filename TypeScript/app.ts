232dcc59d5cf3b0fb6e54dccee56d91d08eb35a1
besevero <besevero@gmail.com>
2018-07-24 00:03:32 -0300

    first commit

---
+++ TypeScript/app.ts
@@ -0,0 +1,13 @@
+let message: string = "Help me Obi-wan-kenobi"
+console.log(message)
+
+let episode: number = 4
+
+console.log("This is episode " + 4)
+
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favouriteDroid: string
+favouriteDroid = 8
+console.log("My favourite droid is " + favouriteDroid)
---
+++ TypeScript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me Obi-wan-kenobi";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favouriteDroid;
+favouriteDroid = 8;
+console.log("My favourite droid is " + favouriteDroid);
