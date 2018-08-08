55cba5350800eedf10da082b0451d1b8d9a1b3ac
besevero <besevero@gmail.com>
2018-08-07 22:29:29 -0300

    Funções

--- TypeScript/dist/app.js
+++ TypeScript/dist/app.js
@@ -5,5 +5,18 @@ console.log("This is episode " + 4);
 episode = episode + 1;
 console.log("Next episode is " + episode);
 var favouriteDroid;
-favouriteDroid = 8;
+favouriteDroid = 'R2';
 console.log("My favourite droid is " + favouriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat the Millenium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call(favouriteDroid);
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc(5,1) = " + inc(5, 1));
+console.log("inc(5)  = " + inc(5));
