85b38fc616f2a7aeb177c82f0a2d09276205e18d
besevero <besevero@gmail.com>
2018-08-07 23:29:29 -0300

    imports

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,38 +1,14 @@
-class Spacecraft {
-  constructor(public propulsor: string){}
-  jumpIntoHyperspace(){
-    console.log(`Entering Hyperspace with ${this.propulsor}`)
 
-  }
-}
+import {Spacecraft, Conteinership} from './base-ships'
+import { MilleniumFalcom} from './starfighters'
 
 let ship = new Spacecraft('Hyperdrive')
 
 ship.jumpIntoHyperspace()
 
-class MilleniumFalcom extends Spacecraft implements Conteinership{
-
-cargoContainers: number
-
-  constructor(){
-    super("Hyperdrive")
-    this.cargoContainers = 4
-  }
-  jumpIntoHyperspace(){
-    if(Math.random() >= 0.5){
-      super.jumpIntoHyperspace()
-    }else{
-      console.log('Failed to jump into hyperspace')
-    }
-  }
-}
-
 let falcon = new MilleniumFalcom()
 falcon.jumpIntoHyperspace()
 
-interface Conteinership{
-  cargoContainers: number
-}
 let goodForTheJob = (ship : Conteinership) => ship.cargoContainers > 2
 
 console.log(`Is Falcon good for the job? ${goodForTheJob(falcon) ? 'Yes': 'No'}`)
--- TypeScript/dist/app.js
+++ TypeScript/dist/app.js
@@ -1,42 +1,10 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Spacecraft = /** @class */ (function () {
-    function Spacecraft(propulsor) {
-        this.propulsor = propulsor;
-    }
-    Spacecraft.prototype.jumpIntoHyperspace = function () {
-        console.log("Entering Hyperspace with " + this.propulsor);
-    };
-    return Spacecraft;
-}());
-var ship = new Spacecraft('Hyperdrive');
+"use strict";
+exports.__esModule = true;
+var base_ships_1 = require("./base-ships");
+var starfighters_1 = require("./starfighters");
+var ship = new base_ships_1.Spacecraft('Hyperdrive');
 ship.jumpIntoHyperspace();
-var MilleniumFalcom = /** @class */ (function (_super) {
-    __extends(MilleniumFalcom, _super);
-    function MilleniumFalcom() {
-        var _this = _super.call(this, "Hyperdrive") || this;
-        _this.cargoContainers = 4;
-        return _this;
-    }
-    MilleniumFalcom.prototype.jumpIntoHyperspace = function () {
-        if (Math.random() >= 0.5) {
-            _super.prototype.jumpIntoHyperspace.call(this);
-        }
-        else {
-            console.log('Failed to jump into hyperspace');
-        }
-    };
-    return MilleniumFalcom;
-}(Spacecraft));
-var falcon = new MilleniumFalcom();
+var falcon = new starfighters_1.MilleniumFalcom();
 falcon.jumpIntoHyperspace();
 var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
 console.log("Is Falcon good for the job? " + (goodForTheJob(falcon) ? 'Yes' : 'No'));
